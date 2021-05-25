6dc273e6367be73dc6850943393ae9b1900e7bdc
Nidula Chithwara <chithwara@gmail.com>
2021-05-25 18:07:20 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:navigation_drawer_example/widget/button_widget.dart';
+import 'package:navigation_drawer_example/widget/navigation_drawer_widget.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String titles = 'Study Rooms Resevation';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: titles,
+        theme: ThemeData(primarySwatch: Colors.purple),
+        home: MainPage(),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        drawer: NavigationDrawerWidget(),
+        // endDrawer: NavigationDrawerWidget(),
+        appBar: AppBar(
+          title: Text(MyApp.titles),
+        ),
+        body: Builder(
+          builder: (context) => Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.symmetric(horizontal: 52),
+            // child: ButtonWidget(
+            //   icon: Icons.open_in_new,
+            //   text: 'Open Drawer',
+            //   onClicked: () {
+            //     Scaffold.of(context).openDrawer();
+            //     // Scaffold.of(context).openEndDrawer();
+            //   },
+            // ),
+          ),
+        ),
+      );
+}
---
+++ lib/page/people_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PeoplePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        //drawer: NavigationDrawerWidget(),
+        appBar: AppBar(
+          title: Text('Booking'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+      );
+}
