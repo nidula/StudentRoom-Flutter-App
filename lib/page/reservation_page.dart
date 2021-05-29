8bc89a5ab56b065d5c470a2c84171a1566d629a1
hivindu <h.amaradeva@gmail.com>
2021-05-29 11:29:01 +0530

    Model classes and service folder creation

--- lib/page/reservation_page.dart
+++ lib/page/reservation_page.dart
@@ -6,57 +6,200 @@ class ResrvationPage extends StatefulWidget {
 }
 
 class _ResrvationPageState extends State<ResrvationPage> {
-TimeOfDay _time = TimeOfDay(hour: 7, minute: 15);
+  DateTime _date = DateTime.now();
 
-void _selectTime() async
-{
-  final TimeOfDay? newTime = await showTimePicker(
+  late String purpose;
+  late int studentId;
+  late int studentCount;
+  late int studyroomId;
+  late DateTime fromTime;
+  late DateTime toTime;
+  late DateTime date;
+  String _slot = "Morning";
+
+  void _selectDate() async {
+    final DateTime? newDate = await showDatePicker(
       context: context,
-      initialTime: _time,
+      initialDate: _date,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2021, 6),
+      helpText: 'Select a date',
     );
-
-  if (newTime != null) {
+    if (newDate != null) {
       setState(() {
-        _time = newTime;
+        _date = newDate;
       });
     }
-}
-late String purpose;
-late int studentId;
-late int studentCount;
-late int studyroomId;
-late DateTime fromTime;
-late DateTime toTime;
-late DateTime date;
+  }
 
-final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-          title: Text('Add Recervation'),
-          centerTitle: true,
-          backgroundColor: Colors.green,
-        ),
-        body: Container(
-          margin: EdgeInsets.all(24),
-          child: 
-            Form(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(),
-                  ElevatedButton(
-              onPressed: _selectTime,
-              child: Text('SELECT TIME'),
-            ),
-                   Text(
-              'Selected time: ${_time.format(context)}',
-            ),
-                ],
-              )
-              ),
-        ),
+      appBar: AppBar(
+        title: Text('Add Recervation'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        margin: EdgeInsets.all(24),
+        child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        DropdownButton(
+                          value: _slot,
+                          style: TextStyle(color: Colors.black, fontSize: 20.0),
+                          dropdownColor: Colors.white,
+                          items: <DropdownMenuItem<String>>[
+                            new DropdownMenuItem(
+                              child: new Text('Morning'),
+                              value: "Morning",
+                            ),
+                            new DropdownMenuItem(
+                              child: new Text('Evening'),
+                              value: "Evening",
+                            ),
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              _slot = value.toString();
+                              if (_slot == "Morning") {
+                                fromTime =
+                                    TimeOfDay(hour: 9, minute: 0) as DateTime;
+                                toTime =
+                                    TimeOfDay(hour: 12, minute: 30) as DateTime;
+                              } else {
+                                fromTime =
+                                    TimeOfDay(hour: 13, minute: 0) as DateTime;
+                                toTime =
+                                    TimeOfDay(hour: 16, minute: 30) as DateTime;
+                              }
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width*0.1,
+                    ),
+                    Column(
+                      children: [
+                        DropdownButton(
+                          value: _slot,
+                          style: TextStyle(color: Colors.black, fontSize: 20.0),
+                          dropdownColor: Colors.white,
+                          items: <DropdownMenuItem<String>>[
+                            new DropdownMenuItem(
+                              child: new Text('101'),
+                              value: "Morning",
+                            ),
+                            new DropdownMenuItem(
+                              child: new Text('102'),
+                              value: "Evening",
+                            ),
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              _slot = value.toString();
+                              if (_slot == "Morning") {
+                                fromTime =
+                                    TimeOfDay(hour: 9, minute: 0) as DateTime;
+                                toTime =
+                                    TimeOfDay(hour: 12, minute: 30) as DateTime;
+                              } else {
+                                fromTime =
+                                    TimeOfDay(hour: 13, minute: 0) as DateTime;
+                                toTime =
+                                    TimeOfDay(hour: 16, minute: 30) as DateTime;
+                              }
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width*0.1,
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(30.0),
+                              child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    shadowColor: Colors.black87,
+                                    shape: new RoundedRectangleBorder(
+                                      borderRadius:
+                                          new BorderRadius.circular(30.0),
+                                    ),
+                                    primary: Colors.greenAccent,
+                                  ),
+                                  onPressed: _selectDate,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Text("Pick the date"),
+                                      SizedBox(
+                                        width: 5.0,
+                                      ),
+                                      Icon(Icons.arrow_drop_down)
+                                    ],
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height*0.05,
+                ),
+                TextFormField(
+                  initialValue: 'Using for......',
+                  maxLength: 50,
+                  validator: (value) {
+                    if (value == null) {
+                      return "Please Neter the value";
+                    } else if (value.length < 10) {
+                      return "Please enter valid input";
+                    }
+                  },
+                  decoration: InputDecoration(
+                    helperText: 'Purpose',
+                    suffixIcon: Icon(
+                      Icons.error,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height*0.5,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                      primary: Colors.greenAccent[400],
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text("Submit"),
+                  ),
+                )
+              ],
+            )),
+      ),
     );
   }
 }
--- lib/page/user_page.dart
+++ lib/page/user_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_drawer_example/widget/reservation_card.dart';
 
 class UserPage extends StatefulWidget {
   @override
@@ -15,7 +16,78 @@ class _UserPageState extends State<UserPage> {
           centerTitle: true,
           backgroundColor: Colors.green,
         ),
-       
+       body: Column(
+         children: [
+           Row(
+             children: [
+               Column(
+                 children: [
+                   Center(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(60),
+                            child: Image.network(
+                              "https://images.pexels.com/photos/2078265/pexels-photo-2078265.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+                              fit: BoxFit.cover,
+                              width: 100,
+                              height: 100,
+                            ),
+                       ),
+                    ),
+                 ],
+               ),
+               SizedBox(
+                width: 10.0,
+              ),
+              Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          "user.name",
+                          style: TextStyle(
+                              fontSize: 22.0,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          "user.nic,",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          "user.area",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                )
+             ],
+           ),
+           SizedBox(
+             height: MediaQuery.of(context).size.height*0.05,
+           ),
+           Text("Reservations", style: TextStyle(
+             fontSize: 18.0,
+             fontWeight: FontWeight.bold
+           ),),
+           SizedBox(
+             height: MediaQuery.of(context).size.height*0.05,
+           ),
+           ReservationCard(),
+         ],
+       ),
       );
   }
 }
