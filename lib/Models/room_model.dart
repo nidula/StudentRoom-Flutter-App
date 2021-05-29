0f3974dd4a480bc9590e0dacded50d43decb1c19
hivindu <h.amaradeva@gmail.com>
2021-05-29 19:53:13 +0530

    Studyroom model

---
+++ lib/Models/room_model.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+StudyRoom issueFromJson(String str) => StudyRoom.fromJson(json.decode(str));
+
+String issueToJson(StudyRoom data) => json.encode(data.toJson());
+
+
+class StudyRoom{
+
+late int sId;
+late int Floor;
+late int capacity;
+late int options;
+late String location;
+
+StudyRoom(
+  this.sId,
+  this.Floor,
+  this.capacity,
+  this.options,
+  this.location,
+);
+
+factory StudyRoom.fromJson(Map<String, dynamic> json)=> StudyRoom(
+   json["sId"],
+   json["floor"],
+   json["capacity"],
+   json["options"],
+   json["location"],
+  );
+
+   Map<String, dynamic> toJson() =>{
+     "sId": sId,
+     "floor":Floor,
+     "capacity":capacity,
+     "options":options,
+     "location":location,
+   };
+
+}
